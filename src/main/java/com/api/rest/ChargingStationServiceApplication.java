2f13baded55d1426c19d9f1cf21b2385d24b1832
cfernandezcairo <cfernandezcairo@gmail.com>
2023-08-05 23:11:12 -0400

    Integrated with spring cloud

--- src/main/java/com/api/rest/ChargingStationServiceApplication.java
+++ src/main/java/com/api/rest/ChargingStationServiceApplication.java
@@ -2,12 +2,14 @@ package com.api.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
-public class PruebasUnitariasSpringBootApplication {
+public class ChargingStationServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PruebasUnitariasSpringBootApplication.class, args);
+        SpringApplication.run(ChargingStationServiceApplication.class, args);
     }
 
 }
--- src/test/java/com/api/rest/ChargingStationServiceApplicationTests.java
+++ src/test/java/com/api/rest/ChargingStationServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PruebasUnitariasSpringBootApplicationTests {
+class ChargingStationServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
