7d78ba454729e78df8dc1cc7444f9d09e0015465
cfernandezcairo <cfernandezcairo@gmail.com>
2023-08-05 21:26:50 -0400

    Update ChargingStation model and all tests

--- src/main/java/com/api/rest/model/ChargingStation.java
+++ src/main/java/com/api/rest/model/ChargingStation.java
@@ -4,6 +4,9 @@ import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
@@ -25,19 +28,28 @@ public class ChargingStation implements Serializable {
     private String chargingStationId;
 
     @Embedded
+    @NonNull
+    @Valid
     private ChargingStationLocation chargingStationLocation;
 
     @Column(name = "type",nullable = false)
+    @NotBlank
+    @NonNull
     private String chargingStationType;
 
     @ElementCollection(fetch = FetchType.LAZY)
     @CollectionTable(name = "charging_station_points", joinColumns = @JoinColumn(name = "chargingStation_id"))
+    @NotEmpty
+    @Valid
     private Set<ChargingStationPoint> chargingStationPoints = new HashSet<>();
 
     @Column(name = "size", nullable = false)
+    @NonNull
     private Integer chargingPointsAmount;
 
     @Column(name = "status",nullable = false)
+    @NotBlank
+    @NonNull
     private String chargingStatus;
 
 }
